20f1c4f0a61e103175b15c0004406337cef242fa
Matteo <person@place.com>
2017-06-29 12:48:41 -0500

    Pushed. Ready for remote server deploy

---
+++ travel_time_visualization/server.py
@@ -0,0 +1,97 @@
+from flask import Flask, jsonify,render_template,request
+from config import API_KEY
+import datetime
+from collections import defaultdict
+import requests
+import pandas as pd
+import sys
+import logging
+from itertools import repeat
+app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+
+from multiprocessing.dummy import Pool as ThreadPool 
+pool = ThreadPool(20) 
+BASE_URL="https://maps.googleapis.com/maps/api/"
+
+class GAPIError(Exception):
+    status_code = 31337
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+def makeRequest(url, API_KEY):
+    url+="&key=%s"%API_KEY
+    return requests.get(url).json()['rows'][0]['elements'][0]['duration_in_traffic']['value']
+def getDistanceMatrix(origin,destination,mode,departure_time,traffic_model, API_KEY):
+    #UTC Time
+    url=BASE_URL+"distancematrix/json?"
+    params="origins=%s&destinations=%s&mode=%s&departure_time=%s&traffic_model=%s"%(origin,destination,mode,departure_time,traffic_model)
+    return makeRequest(url+params, API_KEY)
+
+def getNearest(dt,offset):
+    return dt + (datetime.datetime.min - dt) % datetime.timedelta(minutes=offset)
+
+def getChartData(starting_address,destination_address, leave_after, hours_to_grab,API_KEY,OFFSET=15):
+    start_date=getNearest(leave_after,15)
+    request_times=defaultdict(dict)
+    dts=[int(leave_after.timestamp())]
+    
+    for dt in (start_date + datetime.timedelta(minutes=offset) for offset in range(0,60*hours_to_grab,OFFSET)):
+        dts.append(int(dt.timestamp()))
+    
+    request_times={}
+    for traffic_model in ["best_guess","pessimistic","optimistic"]:
+        results=pool.starmap(
+            getDistanceMatrix, zip(repeat(starting_address),repeat(destination_address),repeat("car"),dts,repeat(traffic_model), repeat(API_KEY))
+        )
+        request_times[traffic_model]=results
+        request_times["index"]=dts
+    travel_times=pd.DataFrame.from_dict(request_times).set_index("index")/60
+    viz_df=travel_times.reset_index()
+    viz_df['x']=viz_df['index']*1000#Add milliseconds for JS datetime
+    del viz_df['index']
+    viz_json=viz_df.to_dict(orient="list")
+    #to c3 Columns
+    columns=[]
+    for col,vals in viz_json.items():
+        if col!="x":
+            vals=[round(x) for x in vals]
+        columns.append([col]+vals)
+    return columns
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+    
+@app.route('/data')
+def data():
+    app.logger.debug(request.args)  
+    leaveAfter=request.args.get("leaveAfter")
+    leaveAfter=datetime.datetime.strptime(leaveAfter,"%Y-%m-%dT%H:%M")
+    USERS_API_KEY=request.args.get("API_KEY",default=API_KEY)
+    if USERS_API_KEY==API_KEY:
+       raise GAPIError("API Key no longer valid", status_code=31337)
+    now=datetime.datetime.now()
+    if leaveAfter<now:
+        leaveAfter=now
+    return jsonify(getChartData(request.args.get("startingAddress"),request.args.get("destinationAddress"),leaveAfter,8, USERS_API_KEY))
+    
+@app.errorhandler(GAPIError)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+    
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=5000)
---
+++ travel_time_visualization/static/main.js
@@ -0,0 +1,257 @@
+function initAutocomplete() {
+  var input_starting = document.getElementById('startingAddress');
+  var input_destination = document.getElementById('destinationAddress');
+  
+  var autocomplete_starting = new google.maps.places.Autocomplete(input_starting);
+  var autocomplete_destination = new google.maps.places.Autocomplete(input_destination);
+  
+  function geolocate() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete_starting.setBounds(circle.getBounds());
+        autocomplete_destination.setBounds(circle.getBounds());
+      });
+    }
+  }
+  var format = d3.time.format("%Y-%m-%dT%H:%M")
+  document.getElementById("leaveAfter").value = format(new Date());
+
+}
+
+
+function showLoader(show){
+     console.log(show)
+    if (show){
+        document.getElementById("loader-1").style.visibility="visible";
+        document.getElementById("loader-1").style.height="100px";
+    } else{
+        document.getElementById("loader-1").style.visibility="hidden";
+        document.getElementById("loader-1").style.height="0px";
+    }
+}
+
+function addGMapsLink(){
+    //format https://www.google.com/maps/dir/Chicago,+IL+60607/Mariano's+Fresh+Market,+40+S+Halsted+St,+Chicago,+IL+60661
+    document.getElementById("gmapsP").style.visibility="visible";
+    document.getElementById("gmapsHref").href="https://www.google.com/maps/dir/"+document.getElementById("startingAddress").value+"/"+document.getElementById("destinationAddress").value;
+}
+
+
+function submit(){
+    var params={}
+    params['startingAddress'] = document.getElementById("startingAddress").value;
+    params['destinationAddress'] = document.getElementById("destinationAddress").value;
+    params['leaveAfter'] = document.getElementById("leaveAfter").value;
+    params['API_KEY'] = document.getElementById("apiKeyInput").value;
+    url_string="/data?"
+    for (var key in params) {
+        if (params[key]){
+            url_string+=key+"="+params[key]+"&"
+        }
+    }
+    console.log(url_string);
+    showLoader(true);
+    d3.json(url_string, function(error, response) {
+        if (error){
+            console.log(error);
+            console.log("ERR");
+            document.getElementById("apiKeyLabel").style.visibility="visible";
+            document.getElementById("apiKeyInput").style.visibility="visible";
+        }else{
+            genChart(response);
+            addGMapsLink();
+            }
+        showLoader(false);
+    });
+}
+
+//So we don't waste free api calls
+function dummySubmit(){
+    var params={}
+    params['startingAddress'] = document.getElementById("startingAddress").value;
+    params['destinationAddress'] = document.getElementById("destinationAddress").value;
+    params['leaveAfter'] = document.getElementById("leaveAfter").value;
+
+    url_string="/data?"
+    for (var key in params) {
+        url_string+=key+"="+params[key]+"&"
+    }
+    
+    console.log(url_string);
+    showLoader(true);
+    setTimeout(function(){
+        genChart(col_data)
+        showLoader(false);
+        }, 3000);
+}
+
+var col_data=[['best_guess',
+  35.0,
+  35.0,
+  34.0,
+  34.0,
+  34.0,
+  34.0,
+  34.0,
+  34.0,
+  35.0,
+  36.0,
+  38.0,
+  43.0,
+  48.0,
+  49.0,
+  52.0,
+  53.0,
+  57.0,
+  57.0,
+  58.0,
+  57.0,
+  57.0,
+  57.0,
+  57.0,
+  55.0,
+  53.0,
+  50.0,
+  48.0,
+  45.0,
+  42.0,
+  39.0,
+  36.0,
+  33.0,
+  32.0],
+ ['optimistic',
+  27.0,
+  27.0,
+  27.0,
+  28.0,
+  28.0,
+  29.0,
+  29.0,
+  30.0,
+  30.0,
+  31.0,
+  32.0,
+  35.0,
+  39.0,
+  40.0,
+  42.0,
+  43.0,
+  46.0,
+  47.0,
+  47.0,
+  46.0,
+  46.0,
+  46.0,
+  46.0,
+  45.0,
+  44.0,
+  42.0,
+  41.0,
+  38.0,
+  36.0,
+  34.0,
+  31.0,
+  30.0,
+  28.0],
+ ['pessimistic',
+  41.0,
+  41.0,
+  41.0,
+  42.0,
+  43.0,
+  43.0,
+  43.0,
+  45.0,
+  46.0,
+  48.0,
+  53.0,
+  61.0,
+  67.0,
+  70.0,
+  74.0,
+  78.0,
+  82.0,
+  83.0,
+  82.0,
+  83.0,
+  83.0,
+  80.0,
+  77.0,
+  73.0,
+  72.0,
+  66.0,
+  62.0,
+  58.0,
+  54.0,
+  50.0,
+  45.0,
+  42.0,
+  40.0],
+ ['x',
+  1498753657000,
+  1498753800000,
+  1498754700000,
+  1498755600000,
+  1498756500000,
+  1498757400000,
+  1498758300000,
+  1498759200000,
+  1498760100000,
+  1498761000000,
+  1498761900000,
+  1498762800000,
+  1498763700000,
+  1498764600000,
+  1498765500000,
+  1498766400000,
+  1498767300000,
+  1498768200000,
+  1498769100000,
+  1498770000000,
+  1498770900000,
+  1498771800000,
+  1498772700000,
+  1498773600000,
+  1498774500000,
+  1498775400000,
+  1498776300000,
+  1498777200000,
+  1498778100000,
+  1498779000000,
+  1498779900000,
+  1498780800000,
+  1498781700000]]
+
+genChart(col_data);
+function genChart(col_data){
+    var chart = c3.generate({
+        data: {
+            x: 'x',
+            xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
+            columns: col_data
+                      
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%I:%M %p'
+                },
+                lable: 'Departure Time'
+            },
+            y: {
+                label: 'Trip Duration (minutes)'
+                },
+                
+        }
+    });
+}
+
