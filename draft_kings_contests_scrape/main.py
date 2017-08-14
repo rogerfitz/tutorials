6a7432d510ea6583f9a5fa95683069a76ddf32b0
Matteo Hoch <minime@hochweb.com>
2017-08-14 11:03:26 -0500

    Final commit

--- draft_kings_contests_scrape/main.py
+++ draft_kings_contests_scrape/main.py
@@ -5,7 +5,7 @@ import random
 from selenium import webdriver
 from config import DK_PASSWORD, DK_USERNAME
 chromeOptions = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : os.getcwd()}
+prefs = {"download.default_directory" : os.getcwd()+"/data"}
 chromeOptions.add_experimental_option("prefs",prefs)
 driver = webdriver.Chrome(chrome_options=chromeOptions)  # Optional argument, if not specified will search path.
 
@@ -51,8 +51,14 @@ for row in table.find("div","grid-canvas").find_all("div","slick-row"):
     rows.append(row_data)
 live_contests=pd.DataFrame(rows)#missing some elements that you need to scroll for
 
-live_contests['time_pulled']=datetime.datetime.now()
+def get_lineups(link):
+    randomSleep(4)
+    BASE_URL="https://www.draftkings.com"
+    driver.get(BASE_URL+link)
+    driver.find_element_by_id("export-lineups-csv").click()
 
+live_contests['time_pulled']=datetime.datetime.now().strftime("%Y-%m-%d %I")
+contests['link'].apply(get_lineups)
 file_name="NFL_contests.csv"
 try:
 	old_contests=pd.read_csv(file_name)
