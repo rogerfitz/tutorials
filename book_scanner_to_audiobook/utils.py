0d45cc4baeb220162d09eb5073e843249169cdba
Matteo Hoch <matteo@hochweb.com>
2022-01-05 12:24:43 -0800

    Added keyframes to OCR, microsoft v good v

---
+++ book_scanner_to_audiobook/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_frame(frame, figsize=(20,20)):
+    #added for grayscale images below
+    shape=np.shape(frame)
+    fig=plt.figure(figsize = figsize)
+    if len(shape)==2:
+        plt.imshow(frame, cmap='gray', vmin=0, vmax=255, interpolation="nearest")
+    else:
+        plt.imshow(frame, interpolation="nearest")
+    plt.show()
